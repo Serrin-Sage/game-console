a42cc9a07ec25eb4b7d28c4e196c0f13be8777d2
Serrin Doscher <serrin.doscher53@myhunter.cuny.edu>
2023-04-30 19:49:06 -0400

    added control legend, not styled yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import GameBoy from './components/GameBoy'
 import ColorPicker from './components/ColorPicker';
+import Legend from './components/Legend';
 function App() {
   useEffect(() => {
     const script = document.createElement('script');
@@ -12,8 +13,13 @@ function App() {
   },[])
   return (
     <div className="App">
-      <GameBoy />
-      <ColorPicker />
+      <div>
+        <GameBoy />
+        <ColorPicker />
+      </div>
+      <div>
+        <Legend />
+      </div>
     </div>
   )
 }
---
+++ src/components/Legend.jsx
@@ -0,0 +1,16 @@
+const Legend = () => {
+  return (
+    <div className="legend-container">
+        Controls <br/>
+        Q = On / Off <br/>
+        W = Up <br/>
+        A = Left <br/>
+        S = Down <br/>
+        D = Right <br />
+        J = Confirm / Select <br />
+        K = Reset {`(When Prompted)`}
+    </div>
+  )
+}
+
+export default Legend

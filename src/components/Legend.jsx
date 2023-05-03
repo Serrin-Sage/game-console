c909caf8378c0af41876b70aec737422efc4f100
Serrin Doscher <serrin.doscher53@myhunter.cuny.edu>
2023-05-02 22:52:59 -0400

    added font to legend, made organization changes

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -1,14 +1,14 @@
 const Legend = () => {
   return (
     <div className="legend-container">
-        Controls <br/>
+        <h2>Controls</h2> <br/>
         Q = On / Off <br/>
         W = Up <br/>
         A = Left <br/>
         S = Down <br/>
         D = Right <br />
         J = Confirm / Select <br />
-        K = Reset {`(When Prompted)`}
+        K = Reset {`When Prompted`}
     </div>
   )
 }

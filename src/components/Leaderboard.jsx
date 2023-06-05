957765afc6d1a43f6b97a6e4890b7ff97f3d7d52
Serrin Doscher <serrin.doscher53@myhunter.cuny.edu>
2023-06-05 12:00:27 -0400

    express server can successfully be pulled from with GET on Frontend

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,24 +1,39 @@
 import { useEffect, useState } from "react"
+import axios from "axios"
+
 const Leaderboard = () => {
 
   const [leaderboardList, setLeaderBoardList] = useState([])
 
+  // useEffect(() => {
+  //     const fetchLeaderBoard = async () => {
+  //       try {
+  //           const response = await fetch("https://serrin-sage.github.io/snake_game_data/leaderboard.json");
+  //           const data = await response.json();
+  //           const sortedData = data.Users.sort((a, b) => b.score - a.score);
+  //           setLeaderBoardList(sortedData);
+  //         } catch (error) {
+  //           console.log("Error fetching leaderboard: ", error);
+  //         }
+  //     }
+
+  //     fetchLeaderBoard()
+  // },[])
+
   useEffect(() => {
-      const fetchLeaderBoard = async () => {
-        try {
-            const response = await fetch("https://serrin-sage.github.io/snake_game_data/leaderboard.json");
-            const data = await response.json();
-            const sortedData = data.Users.sort((a, b) => b.score - a.score);
-            setLeaderBoardList(sortedData);
-          } catch (error) {
-            console.log("Error fetching leaderboard: ", error);
-          }
-      }
-
-      fetchLeaderBoard()
-  },[])
-
+    getLeaderboard()
+  }, [])
   
+  const getLeaderboard = async () => {
+    const response = await axios.get("http://localhost:5000/leaderboard")
+    const data = response.data
+    if (response.status === 200) {
+      const sortedData = data.Users.sort((a, b) => b.score - a.score);
+      setLeaderBoardList(sortedData)
+    } else {
+      // console.log(response)
+    }
+  }
   
   return (
     <div>
